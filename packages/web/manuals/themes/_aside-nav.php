8283e968af457e5882e095186c22760dafd7aa46
Hassadee Pimsuwan <h.pimsuwan@gmail.com>
2018-04-25 19:12:11 +0700

    Add manual document menu theme

---
+++ packages/web/manuals/themes/_aside-nav.php
@@ -0,0 +1,93 @@
+<ul>
+	<li>Contribute</li>
+	<li>
+		Quick Reference
+		<ul>
+			<li>Primitive Shapes</li>
+			<li>Shape Transformations</li>
+			<li>Shape Operations</li>
+			<li>Extrusions/Projections</li>
+			<li>Design Parameters</li>
+			<li>General</li>
+		</ul>
+	</li>
+	<li>
+		User Guide/คู่มือผู้ใช้
+		<ul>
+			<li>Website/เว็บไซต์</li>
+			<li>Addendum</li>
+		</ul>
+	</li>
+	<li>
+		Design Guide/คู่มือการออกแบบ
+		<ul>
+			<li>Anatomy of a Design</li>
+			<li>
+				3D Primitives
+				<ul>
+					<li>Cube/ทรงลูกบาศก์</li>
+					<li>Sphere/ทรงกลม</li>
+					<li>Cylinder/ทรงกระบอก</li>
+					<li>Torus</li>
+					<li>Polyhedron</li>
+				</ul>
+			</li>
+			<li>
+				2D Primitives
+				<ul>
+					<li>Rectangle</li>
+					<li>Circle</li>
+					<li>Polygon</li>
+				</ul>
+			</li>
+			<li>
+				Other Primitives
+				<ul>
+					<li>Path</li>
+					<li>Text</li>
+				</ul>
+			</li>
+			<li>
+				Transforms
+				<ul>
+					<li>Rotate</li>
+					<li>Scale</li>
+					<li>Translate</li>
+					<li>Center</li>
+					<li>Mirror</li>
+					<li>Matrix Transform</li>
+					<li>Color/สี</li>
+				</ul>
+			</li>
+			<li>
+				Operations
+				<ul>
+					<li>Union</li>
+					<li>Interaction</li>
+					<li>Difference</li>
+					<li>Hull</li>
+					<li>Chain Hull</li>
+				</ul>
+			</li>
+			<li>
+				Extrusions/Projections
+				<ul>
+					<li>Linear Extrude</li>
+					<li>Rectangular Extrude</li>
+					<li>Rotate Extrude</li>
+				</ul>
+			</li>
+			<li>
+				Shape Attributes
+				<ul>
+					<li>Properties</li>
+					<li>Measurements</li>
+					<li>Connectors</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		Math Functions/ฟังก์ชันทางคณิตศาสตร์
+	</li>
+</ul>
